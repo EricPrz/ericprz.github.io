136eb76c59b863e90c197df37724c46c981ac8ad
Eric <neryo19media@gmail.com>
2022-07-31 11:27:24 +0200

    ns

--- js/android.js
+++
@@ -1,7 +0,0 @@
-function calcVH() {
-    var vH = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    document.getElementById("hero").setAttribute("style", "height:" + vH + "px;");
-  }
-  calcVH();
-  window.addEventListener('onorientationchange', calcVH, true);
-  window.addEventListener('resize', calcVH, true);
