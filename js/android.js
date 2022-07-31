cddebd520c849425f5635e6446c60e6e7bd13f7d
Eric <neryo19media@gmail.com>
2022-07-31 10:38:30 +0200

    Prueba

--- js/android.js
+++ js/android.js
@@ -1,6 +1,6 @@
 function calcVH() {
     var vH = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    document.getElementsByClassName("hero").setAttribute("style", "height:" + vH + "px;");
+    document.getElementById("hero").setAttribute("style", "height:" + vH + "px;");
   }
   calcVH();
   window.addEventListener('onorientationchange', calcVH, true);
